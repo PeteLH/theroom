c019c21c39e6ed37b502032d757bb83641e1fdc7
PETE_LAPTOP <Pete_Harries@hotmail.co.uk>
2017-01-16 18:33:07 +0000

    [ADD} UI work and new lamp asset matts

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -1,23 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class MenuHandler : MonoBehaviour {
 
     public Camera fpsCam;
     public Camera menuCam;
+    public Canvas HUD;
+    public Canvas MainMenu;
+    public GameObject player;
 
 	// Use this for initialization
 	void Start ()
     {
         fpsCam = GameObject.Find("FPSController").GetComponentInChildren < Camera > ();
-        menuCam = gameObject.GetComponent<Camera>();
+        menuCam = GameObject.Find("MainMenuCam").GetComponentInChildren<Camera>();
 
         menuCam.enabled = true;
         fpsCam.enabled = false;
+        HUD.enabled = false;
+        MainMenu.enabled = true;
     }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void StartGame()
+    {
+        menuCam.enabled = false;
+        fpsCam.enabled = true;
+
+        HUD.enabled = true;
+        MainMenu.enabled = false;
+
+        player.GetComponent<FirstPersonController>().enabled = true;
+    }
 }
