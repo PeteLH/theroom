97c8eead8de580cd5c530e2451a8a0f6869ad5d6
DESKTOP-B9G4QSH\Pete <Pete_Harries@hotmail.co.uk>
2017-01-16 10:54:32 +0000

    [ADD] new assets and prefabs

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour {
+
+    public Camera fpsCam;
+    public Camera menuCam;
+
+	// Use this for initialization
+	void Start ()
+    {
+        fpsCam = GameObject.Find("FPSController").GetComponentInChildren < Camera > ();
+        menuCam = gameObject.GetComponent<Camera>();
+
+        menuCam.enabled = true;
+        fpsCam.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
