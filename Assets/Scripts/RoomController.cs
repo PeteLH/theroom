73d469efe179272ebfac109860560b979a88f9a2
PETE_LAPTOP <Pete_Harries@hotmail.co.uk>
2017-01-27 21:13:25 +0000

    [MOD] work on room flow

--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -1,17 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RoomController : MonoBehaviour
 {
+    public BoxCollider room1Trigger;
+    public BoxCollider room2Trigger;
+    public BoxCollider room3Trigger;
+    public BoxCollider room4Trigger;
+
     public Transform[] spawnPoints;
     public GameObject[] roomPresets;
+    public List<GameObject> currentRooms = new List<GameObject>();
 
     void Start()
     {
         for (var i = 0; i < spawnPoints.Length; i++)
         {
-            Instantiate(roomPresets[0], spawnPoints[i].position, spawnPoints[i].rotation);
+            //currentRooms.Add();
+            //(Instantiate(roomPresets[0], spawnPoints[i].position, spawnPoints[i].rotation));
+            
         }
+    }
+
+    void OnCollisionEnter (Collision room1Trigger)
+    {
+
     }
 
     public void ClearRooms()
