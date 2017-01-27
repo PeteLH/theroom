b81c709608c560f5ce95bcc89fac598df7773fd6
PETE_LAPTOP <Pete_Harries@hotmail.co.uk>
2017-01-27 18:21:59 +0000

    [ADD] script to control room populations

---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomController : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject[] roomPresets;
+
+    void Start()
+    {
+        for (var i = 0; i < spawnPoints.Length; i++)
+        {
+            Instantiate(roomPresets[0], spawnPoints[i].position, spawnPoints[i].rotation);
+        }
+    }
+
+    public void ClearRooms()
+    {
+
+    }
+}
+
+//Transform item in spawnPoints[]
