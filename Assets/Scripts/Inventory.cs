b8cecec9e5b66860303938cc7c8341f4226b6c5e
PETE_DESKTOP_HOME <Pete_Harries@hotmail.co.uk>
2017-02-28 00:24:05 +0000

    [MOD] UI work

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+    public Canvas inventory;
+
+	// Use this for initialization
+	void Start ()
+    {
+        inventory.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            if (inventory.enabled == true)
+            {
+                inventory.enabled = false;
+            }
+            else
+            {
+                inventory.enabled = true;
+            }
+        }
+    }
+}
