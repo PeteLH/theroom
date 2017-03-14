be569c54957eb905f6d3dd18255c9b32e4d687aa
PETE_DESKTOP_HOME <Pete_Harries@hotmail.co.uk>
2017-03-14 23:07:46 +0000

    [ADD]Prologue section

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -13,8 +13,15 @@ public class MenuHandler : MonoBehaviour {
     public GameObject player;
     public GameObject inventoryController;
     public AudioSource MainMenuMusic;
+    public AudioSource sting;
     public AudioClip UiClick;
     public AudioClip GameStartSting;
+    public Animator prologueAnimator;
+    public Text prologueText;
+    public GameObject playerNameEntry;
+
+    public Canvas prologue;
+
     bool hasSeenTut = false;
 
     // Use this for initialization
@@ -37,18 +44,80 @@ public class MenuHandler : MonoBehaviour {
     {
 	if (hasSeenTut == false & Input.GetKeyDown("e") & tutorial.enabled == true)
         {
-            StartGame();
+            startPrologue();
         }
-	}
+
+        if (Input.GetMouseButtonDown(0) & onPrologue == true)
+        {
+            advancePeologue();
+        }
+    }
     public void showTut()
     {
         MainMenu.enabled = false;
         tutorial.enabled = true;
+        onGameStartSting();
+    }
+
+    bool onPrologue = false;
+    public void startPrologue()
+    {
+        onPrologue = true;
+        MainMenuMusic.mute = true;
+        tutorial.enabled = false;
+        prologue.enabled = true;
+        prologueAnimator.SetTrigger("FadeIn");
+        prologueText.text = "Whatever you do, do not open your eyes.";
+    }
+
+    public int prologueStage;
+    public string playerName;
+    public void advancePeologue()
+    {
+        switch (prologueStage)
+        {
+            case 0:
+                prologueText.text = "What is your name?";
+                playerNameEntry.SetActive(true);
+                prologueStage++;
+                break;
+
+            case 1:
+                if(playerNameEntry.GetComponent<InputField>().text != "")
+                {
+                    playerName = playerNameEntry.GetComponent<InputField>().text;
+                    playerNameEntry.SetActive(false);
+                    prologueText.text = "You strayed from the straight path " + playerName + ", and now are lost in this dark wood.";
+                    prologueStage++;
+                }
+                break;
+
+            case 2:
+                prologueText.text = "I cannt guide you. But will restore your vision.";
+                prologueStage++;
+                break;
+
+            case 3:
+                prologueText.text = playerName + " look for any clue that will lead you away from here.";
+                prologueStage++;
+                break;
+
+            case 4:
+                prologueText.text = "Or it will find you...";
+                prologueStage++;
+                break;
+
+            case 5:
+                prologueText.text = "";
+                StartGame();
+                break;
+        }
     }
 
     public void StartGame()
     {
-        tutorial.enabled = false;
+        prologue.enabled = false;
+        onPrologue = false;
         menuCam.enabled = false;
         fpsCam.enabled = true;
 
@@ -59,7 +128,6 @@ public class MenuHandler : MonoBehaviour {
         lockCursor();
         inventoryController.GetComponent<Inventory>().onMainMenu = false;
         inventoryController.GetComponent<Inventory>().lockbuttons();
-        onGameStartSting();
     }
 
     public void lockCursor()
@@ -81,7 +149,6 @@ public class MenuHandler : MonoBehaviour {
 
     public void onGameStartSting()
     {
-        MainMenuMusic.PlayOneShot(GameStartSting, 1f);
-        //MainMenuMusic.mute = true;
+        sting.PlayOneShot(GameStartSting, 1f);
     }
 }
